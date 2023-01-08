7497bc0c2efb0983c10ff0c007586eb56e99a244
Tosin2289 <ayoolatosin00@gmail.com>
2023-01-08 15:59:05 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,192 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 350,
+          width: MediaQuery.of(context).size.width * .9,
+          child: Card(
+            color: Colors.white,
+            elevation: 30,
+            child: Container(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.blue.withOpacity(0.2),
+                                child: Image.asset(
+                                  'assets/sendicon.jpg',
+                                  height: 30,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Send",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.cyan.withOpacity(0.2),
+                                child: Image.asset(
+                                  'assets/payicon.png',
+                                  height: 30,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Pay",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.amber.withOpacity(0.2),
+                                child: Image.asset(
+                                  'assets/requesticon.png',
+                                  height: 30,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Request",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.pink.withOpacity(0.2),
+                                child: Image.asset(
+                                  'assets/invoiceicon.png',
+                                  height: 30,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "invoice",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.purple.withOpacity(0.2),
+                                child: Image.asset(
+                                  'assets/charityicon.png',
+                                  height: 30,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Charity",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor:
+                                    Colors.deepPurple.withOpacity(0.2),
+                                child: Image.asset(
+                                  'assets/loanicon.png',
+                                  height: 30,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Loan",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Divider(
+                        height: 1,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "We keep our money in \nan international back.",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.grey),
+                            ),
+                            Spacer(),
+                            Container(
+                              height: 50,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                  color: Colors.cyan.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
